d83a9fc7af710277532787cbf31d97f63205bc94
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-11-16 22:22:32 -0500

    remove pry binding

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -2,7 +2,6 @@ require 'digest'
 
 module PeopleHelper
   def gravatar_url_for(email)
-    binding.pry
     hash = Digest::MD5.digest(email.downcase.strip)
     "http://www.gravatar.com/avatar/" + hash
   end
